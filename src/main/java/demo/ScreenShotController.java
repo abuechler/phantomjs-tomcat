8fae08392f5b5a2f7f5690f73fa5bd658b3f374c
Andi Büchler <andi.buechler@gmail.com>
2015-02-27 16:04:28 +0100

    Inital

---
+++ src/main/java/demo/ScreenShotController.java
@@ -0,0 +1,26 @@
+package demo;
+
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScreenShotController {
+
+	@Autowired
+	PhantomJSDriver webDriver;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+	public @ResponseBody byte[] takeScreenShot(@RequestParam(defaultValue="http://www.google.com") String url) throws IOException {
+		webDriver.get(url);
+		return webDriver.getScreenshotAs(OutputType.BYTES);
+	}
+}
---
+++ src/main/java/demo/SpringConfig.java
@@ -0,0 +1,37 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+	public static final String PHANTOMJS = "/Users/andi/Downloads/phantomjs";
+
+	
+	@Bean(destroyMethod = "quit")
+	public PhantomJSDriver webDriver() {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setJavascriptEnabled(true);
+		caps.setCapability("takesScreenshot", true);
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, PHANTOMJS);
+		PhantomJSDriver driver = new PhantomJSDriver(caps);
+		driver.setLogLevel(Level.WARNING);
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+		driver.manage().window().setSize(new Dimension(1920, 1200));
+		return driver;
+	}
+}
